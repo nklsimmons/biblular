ef6d0811424a99fc65e01bb25c98c7e43cf84675
Nathaniel Simmons <nkl.simmons@proton.me>
2024-08-20 16:28:43 +0800

    Twig

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.set('view engine', 'twig');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { MongoClient } = require("mongodb");
+const Twig = require("twig");
 
 const uri = "mongodb://localhost:27117";
 const client = new MongoClient(uri);
@@ -10,16 +11,21 @@ const database = client.db('biblular');
 const versesdb = database.collection('verses');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+// /* Show results */
+// router.get('/results', function(req, res) {
+
+// });
 
 /* POST */
 router.post('/', function(req, res) {
   const s = req.body.s;
   const s_arr = s.split(' ');
 
-  let count = 0;
+  let nInTheBible = 0;
 
   Promise.all(s_arr.map(async (s) => {
 
@@ -30,18 +36,21 @@ router.post('/', function(req, res) {
   }))
   .then(results => {
     for(let result of results) {
-      if(result[0]) {
-        count++;
-        console.log('"' + result[1] + '"' + " is in the bible")
-      } else {
-        console.log('"' + result[1] + '"' + " is not in the bible")
-      }
+      if(result[0])
+        nInTheBible++;
     }
+    return results;
   })
-  .then(_ => {
-    // console.log(count)
-    res.send(`${count} of those words are in the bible.`)
-  });  
+  .then(results => {
+    // console.log(nInTheBible)
+    // res.send(`${nInTheBible} of those words are in the bible.`)
+
+    console.log([nInTheBible, results, s])
+
+    let nNotInTheBible = results.length - nInTheBible;
+
+    res.render('result.twig', {nInTheBible, nNotInTheBible, results, s});
+  });
 });
 
 module.exports = router;
